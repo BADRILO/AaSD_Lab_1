ab68bc37a5001ce22d908f82b4bab630f936975c
DESKTOP-II130FV\DIGAICIA_BADRILO <artem.dehtiarov@nure.ua>
2021-03-15 18:26:10 +0200

    Changed the case of Node`s fields

--- Lab_1/Node.cs
+++ Lab_1/Node.cs
@@ -4,13 +4,13 @@ namespace Lab_1
 {
     class Node
     {
-        public int data;
-        public Node next;
+        public int Data;
+        public Node Next;
 
         public Node(int data, Node next)
         {
-            this.data = data;
-            this.next = next;
+            this.Data = data;
+            this.Next = next;
         }
     }
 }
