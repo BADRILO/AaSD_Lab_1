b757ddda69c51609ba66da9ff092ed58ee1e550f
DESKTOP-II130FV\DIGAICIA_BADRILO <artem.dehtiarov@nure.ua>
2021-03-15 19:35:14 +0200

    Added methods addItem(), printList() and test in Program.cs

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -6,6 +6,14 @@ namespace Lab_1
     {
         static void Main(string[] args)
         {
+            SortedLinkedList example = new SortedLinkedList();
+            example.addItem(10);
+            example.addItem(5);
+            example.addItem(20);
+            example.addItem(15);
+            example.addItem(2);
+            example.addItem(30);
+            example.printList();
         }
     }
 }
--- Lab_1/SortedLinkedList.cs
+++ Lab_1/SortedLinkedList.cs
@@ -12,5 +12,41 @@ namespace Lab_1
             this.len = 0;
             this.first = null;
         }
+        public void addItem(int item)
+        {
+            if (len == 0 || item < first.Data)
+            {
+                Node node = new Node(item, first);
+                first = node;
+                len++;
+            }
+            else
+            {
+                Node lower_node  = first;
+                Node bigger_node = first.Next;
+
+                while (true)
+                {
+                    if (bigger_node == null || item < bigger_node.Data)
+                    {
+                        Node node = new Node(item, bigger_node);
+                        lower_node.Next = node;
+                        len++;
+                        break;
+                    }
+                    bigger_node = bigger_node.Next;
+                    lower_node = lower_node.Next;
+                }
+            }
+        }
+        public void printList()
+        {
+            Node pointer = first;
+            for (int i = 1; pointer != null;)
+            {
+                Console.WriteLine($"Node №{i++}: {pointer.Data}");
+                pointer = pointer.Next;
+            }
+        }
     }
 }
